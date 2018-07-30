fdc8e803f95bee23b7027a06d0700ffd2ebf4006
YoganshGupta <41914199+YoganshGupta@users.noreply.github.com>
2018-07-30 21:37:01 +0530

    It is a quiz game with questions of various fields to be answered to win prize. For this user has to undergo through some twists

---
+++ GenTechQuiz/src/gentechquiz/Main.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gentechquiz;
+
+/**
+ *
+ * @author Admin
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static float a=0;
+    public static float b=0;
+    public static float c=0;
+    public static float d=0;
+    public static float e=0;
+    public static float f=0;
+    public static float g=0;
+    public static float h=0;
+    public static float i=0;
+    public static float j=0;
+    public static float k=0;
+    public static float l=0;
+    public static float m=0;
+    
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
